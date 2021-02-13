a956d562d925c4d013174d0bc09bdc3c02611051
Alejandro Villa <alexandros17.sk@gmail.com>
2021-02-12 18:23:46 -0600

    handling errors

--- app.js
+++ app.js
@@ -8,5 +8,14 @@ const app = express();
 
 app.use('/api/places', placesRoutes);
 
+app.use((error, req, res, next) => {
+    if (res.headerSent) {  //check if a response has already been sent
+        return next(error);
+    };
+
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'An unknown error accurred!' });
+});
+
 
 app.listen(5000);
