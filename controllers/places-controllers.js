69c07547d7115c940c6a24d3c855fc158ed73e9a
Alejandro Villa <alexandros17.sk@gmail.com>
2021-02-16 09:26:09 -0600

    Geocoding google API added to convert address into coordinates

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -2,6 +2,7 @@ const uuid = require('uuid').v4;
 const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
+const getCoordsForAddress = require('../util/location');
 
 let DUMMY_PLACES = [
     {
@@ -58,14 +59,20 @@ const getPlacesByUserId = (req, res, next) => {
     res.json({ places });
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        console.log(errors);
-        throw new HttpError('Invalid inputs passed, please check your data.', 422);
+        return next(new HttpError('Invalid inputs passed, please check your data.', 422));
     };
 
-    const { title, description, coordinates, address, creator } = req.body;
+    const { title, description, address, creator } = req.body;
+
+    let coordinates;
+    try {
+        coordinates = await getCoordsForAddress(address);
+    } catch (error) {
+        return next(error);
+    };
 
     const createdPlace = {
         id: uuid(),
@@ -103,7 +110,7 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
     const placeId = req.params.pid;
-    if(!DUMMY_PLACES.find(p => p.id === placeId)) {
+    if (!DUMMY_PLACES.find(p => p.id === placeId)) {
         throw new HttpError('Could not find a place for that id.', 404);
     };
     DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
