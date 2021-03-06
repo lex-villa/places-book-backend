8ef3417d88fd57781fe5fd6a201ea4ce38355418
Alejandro Villa <alexandros17.sk@gmail.com>
2021-03-06 15:43:27 -0600

    using Multer to save files

--- app.js
+++ app.js
@@ -1,53 +1,59 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const placesRoutes = require('./routes/places-routes');
-const usersRoutes = require('./routes/users-routes');
-const HttpError = require('./models/http-error');
-
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow any domain
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-    
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow any domain
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
 });
 
-app.use('/api/places', placesRoutes);
-app.use('/api/users', usersRoutes);
+app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
-    const error = new HttpError('Could not find this route.', 404);
-    throw error;
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
 });
 
 app.use((error, req, res, next) => {
-    if (res.headerSent) {  //check if a response has already been sent
-        return next(error);
-    };
+  if (res.headerSent) {
+    //check if a response has already been sent
+    return next(error);
+  }
 
-    res.status(error.code || 500);
-    res.json({ message: error.message || 'An unknown error accurred!' });
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error accurred!" });
 });
 
 const connectConfig = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-}
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+};
 
 mongoose
-    .connect('mongodb+srv://mr-robot:conpass1@cluster0.xjeaf.mongodb.net/places?retryWrites=true&w=majority', connectConfig)
-    .then(() => {
-        console.log('Database connected :)')
-        app.listen(5000);
-    })
-    .catch((error) => {
-        console.log(error);
-    }); 
+  .connect(
+    "mongodb+srv://mr-robot:conpass1@cluster0.xjeaf.mongodb.net/places?retryWrites=true&w=majority",
+    connectConfig
+  )
+  .then(() => {
+    console.log("Database connected :)");
+    app.listen(5000);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const { check } = require('express-validator');
+const express = require("express");
+const { check } = require("express-validator");
 
-const usersControllers = require('../controllers/users-controllers');
-
+const usersControllers = require("../controllers/users-controllers");
+const fileUpload = require("../middleware/file-upload");
 
 const router = express.Router();
 
-router.get('/', usersControllers.getUsers);
+router.get("/", usersControllers.getUsers);
 
 router.post(
-    '/signup',
-    [
-        check('name').trim().notEmpty(),
-        check('email').normalizeEmail()  // Test@test.com => test@test.com
-            .isEmail(),
-        check('password').isLength({ min: 6 })
-    ],
-    usersControllers.signup
+  "/signup",
+  fileUpload.single("image"),
+  [
+    check("name").trim().notEmpty(),
+    check("email")
+      .normalizeEmail() // Test@test.com => test@test.com
+      .isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  usersControllers.signup
 );
 
-router.post('/login', usersControllers.login);
+router.post("/login", usersControllers.login);
 
 module.exports = router;
