62a1b20c2b18d410eca7f117ff101a0ab66b1285
Alejandro Villa <alexandros17.sk@gmail.com>
2021-03-06 15:55:06 -0600

    filtering files, images only with the correct format can pass

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -17,6 +17,11 @@ const fileUpload = multer({
       const ext = MIME_TYPE_MAP[file.mimetype];
       cb(null, uuid() + "." + ext);
     },
+    fileFilter: (req, file, cb) => {
+      const isValid = !!MIME_TYPE_MAP[file.mimetype]; // convert undefined or null to false, and some type found into true
+      let error = isValid ? null : new Error("Invalid mime type!");
+      cb(error, isValid);
+    },
   }),
 });
 
