40e99ff1c99d5dc8ac20a96c312b61e62e4694ba
Alejandro Villa <alexandros17.sk@gmail.com>
2021-03-06 19:45:46 -0600

    uploading images for new places

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,34 +1,35 @@
-const express = require('express');
-const { check } = require('express-validator');
+const express = require("express");
+const { check } = require("express-validator");
 
-const placesControllers = require('../controllers/places-controllers');
-
+const placesControllers = require("../controllers/places-controllers");
+const fileUpload = require("../middleware/file-upload");
 
 const router = express.Router();
 
-router.get('/:pid', placesControllers.getPlaceById);
+router.get("/:pid", placesControllers.getPlaceById);
 
-router.get('/user/:uid', placesControllers.getPlacesByUserId);
+router.get("/user/:uid", placesControllers.getPlacesByUserId);
 
 router.post(
-    '/',
-    [
-        check('title').trim().notEmpty(),
-        check('description').trim().isLength({ min: 5 }),
-        check('address').trim().notEmpty(),
-    ],
-    placesControllers.createPlace
+  "/",
+  fileUpload.single("image"),
+  [
+    check("title").trim().notEmpty(),
+    check("description").trim().isLength({ min: 5 }),
+    check("address").trim().notEmpty(),
+  ],
+  placesControllers.createPlace
 );
 
 router.patch(
-    '/:pid',
-    [
-        check('title').trim().notEmpty(),
-        check('description').trim().isLength({ min: 5 })
-    ],
-    placesControllers.updatePlace
+  "/:pid",
+  [
+    check("title").trim().notEmpty(),
+    check("description").trim().isLength({ min: 5 }),
+  ],
+  placesControllers.updatePlace
 );
 
-router.delete('/:pid', placesControllers.deletePlace);
+router.delete("/:pid", placesControllers.deletePlace);
 
 module.exports = router;
